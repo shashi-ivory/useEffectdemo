7a57babbc6e55de4af7dad914d69bd60e502da7b
Hardik <aniketkumar.ss@smartdatainc.net>
2023-05-17 09:44:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setcount] = useState(0);
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [count]);
+  useEffect(() => {
+    console.log("useffect 2");
+  });
+  useEffect(() => {
+    console.log("useEffect 3");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useEffect</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setcount(count + 1)}>Incresce</button>
+      <button onClick={() => setcount(count - 1)}>Decresce</button>
     </div>
   );
 }
